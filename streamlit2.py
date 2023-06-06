171364900cb4d622559501d72bdd328d0c201fc7
MSebastian3 <131408192+MSebastian3@users.noreply.github.com>
2023-06-05 19:25:32 -0600

    Update streamlit2.py

--- streamlit2.py
+++ streamlit2.py
@@ -12,10 +12,19 @@ import pandas as pd
 
 st.title("Police Incident Reports from 2018 to 2020 in San Francisco")
 
-df= pd.read_csv("https://drive.google.com/file/d/11oLcKiW8SgCOp3tGiQCYuRG7pLL_J-Zf/view?usp=drive_link")
+# Provide the direct link to the CSV file
+csv_url = "https://drive.google.com/uc?id=11oLcKiW8SgCOp3tGiQCYuRG7pLL_J-Zf"
+
+# Read the CSV file into a DataFrame
+df = pd.read_csv(csv_url)
 
 st.markdown("The data shown below belongs to incident reports in the city of San Francisco, from the year 2018 to 2020, with details from each case such as date, day of the week, police district, neighborhood in which it happened, type of incident in category and subcategory, exact location and resolution.")
 
-mapa=pd.DataFrame()
-mapa=mapa.dropna()
-st.map(mapa.astype(int))
+# Display the DataFrame
+st.write(df)
+
+# Display the map using latitude and longitude columns from the DataFrame
+if "Latitude" in df.columns and "Longitude" in df.columns:
+    st.map(df[["Latitude", "Longitude"]].dropna())
+else:
+    st.warning("Latitude and Longitude columns are missing in the DataFrame.")
