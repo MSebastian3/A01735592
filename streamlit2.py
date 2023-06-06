9cc9c1546d2052eae16c8651adc971f3588d5c46
MSebastian3 <131408192+MSebastian3@users.noreply.github.com>
2023-06-05 19:28:15 -0600

    Update streamlit2.py

--- streamlit2.py
+++ streamlit2.py
@@ -12,19 +12,19 @@ import pandas as pd
 
 st.title("Police Incident Reports from 2018 to 2020 in San Francisco")
 
-# Provide the direct link to the CSV file
-csv_url = "https://drive.google.com/uc?id=11oLcKiW8SgCOp3tGiQCYuRG7pLL_J-Zf"
-
-# Read the CSV file into a DataFrame
-df = pd.read_csv(csv_url)
+df = pd.read_csv("https://drive.google.com/uc?id=11oLcKiW8SgCOp3tGiQCYuRG7pLL_J-Zf")
 
 st.markdown("The data shown below belongs to incident reports in the city of San Francisco, from the year 2018 to 2020, with details from each case such as date, day of the week, police district, neighborhood in which it happened, type of incident in category and subcategory, exact location and resolution.")
 
 # Display the DataFrame
 st.write(df)
 
-# Display the map using latitude and longitude columns from the DataFrame
+# Check if Latitude and Longitude columns exist
 if "Latitude" in df.columns and "Longitude" in df.columns:
-    st.map(df[["Latitude", "Longitude"]].dropna())
+    # Create a new DataFrame with non-null Latitude and Longitude values
+    mapa = df.dropna(subset=["Latitude", "Longitude"])[["Latitude", "Longitude"]]
+    # Display the map
+    st.map(mapa.astype(float))
 else:
     st.warning("Latitude and Longitude columns are missing in the DataFrame.")
+
