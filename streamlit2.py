73a1433bd9b9ecc91058786dda4d8fe6b314960d
MSebastian3 <131408192+MSebastian3@users.noreply.github.com>
2023-06-06 18:40:35 -0600

    Update streamlit2.py

--- streamlit2.py
+++ streamlit2.py
@@ -12,7 +12,7 @@ import pandas as pd
 
 st.title("Police Incident Reports from 2018 to 2020 in San Francisco")
 
-df= pd.read_csv("Police_Department_Incident_Reports__2018_to_Present.csv")
+df= pd.read_csv("https://drive.google.com/drive/u/1/folders/1Hd2HOFknGccrm11VSdDp5N9-ADkboWvZ")
 
 st.markdown("The data shown below belongs to incident reports in the city of San Francisco, from the year 2018 to 2020, with details from each case such as date, day of the week, police district, neighborhood in which it happened, type of incident in category and subcategory, exact location and resolution.")
 
@@ -27,8 +27,9 @@ mapa['Incident Subcategory'] = df['Incident Subcategory']
 mapa['Resolution'] = df['Resolution']
 mapa['lat'] = df['Latitude']
 mapa['lon'] = df['Longitude']
-
+mapa = pd.DataFrame(['Latitude'],df[Longitude])
 mapa = mapa.dropna()
+
 
 
 
